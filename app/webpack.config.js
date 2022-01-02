02a4d1c863b4c489126c7d068314b4cd9997ddff
Anna-Sokolowska <anna.sokolowska343@gmail.com>
2022-01-02 14:16:13 +0100

    Utworzenie widoku głównego panelu użytkownika

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -22,6 +22,7 @@ Encore
      */
     // .addEntry('app', './assets/app.js')
     .addEntry('auth', './assets/pages/auth.js')
+    .addEntry('dashboard', './assets/pages/dashboard.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
