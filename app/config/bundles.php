122d403b655e5270958e5ff9a86dcd4309333477
MichalMyskow <michal.myskow@stud.pwsz.legnica.edu.pl>
2022-01-03 03:18:28 +0100

    Instalacja i konfiguracja mailera, dodanie funkcjonalności resetowania zapomnianego hasła przy pomocy paczki symfonycasts/reset-password-bundle.

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -15,4 +15,5 @@ return [
     HWI\Bundle\OAuthBundle\HWIOAuthBundle::class => ['all' => true],
     Http\HttplugBundle\HttplugBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
