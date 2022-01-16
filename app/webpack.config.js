459f44f798e50d83c62b53d9f898655ddc9f3416
Anna-Sokolowska <anna.sokolowska343@gmail.com>
2022-01-16 14:58:53 +0100

    Dodanie styli dla zarządzania budżetem

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -26,6 +26,7 @@ Encore
     .addEntry('guest_list', './assets/pages/guest-list.js')
     .addEntry('checklist', './assets/pages/checklist.js')
     .addEntry('guest_placement', './assets/pages/guest-placement.js')
+    .addEntry('budget', './assets/pages/budget.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
