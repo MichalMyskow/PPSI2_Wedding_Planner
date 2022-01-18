9c2c494fd19f17980dcbb0a3da6396832821020c
Anna-Sokolowska <anna.sokolowska343@gmail.com>
2022-01-18 04:59:37 +0100

    Dodanie komunikatu dla podglądu przypisanego miejsca gościa

--- app/assets/js/guestPlacementView.js
+++ app/assets/js/guestPlacementView.js
@@ -1,8 +1,9 @@
 window.addEventListener('load', function(){
-    let viewingGuestSeatId = document.querySelector('#viewing-guest-seat-id').value;
-    console.log(viewingGuestSeatId);
-    let viewingGuestSeat = document.querySelectorAll(`.guest-placement__seat[data-seat-id="${viewingGuestSeatId}"]`)[0];
-    console.log(viewingGuestSeat);
-
-    viewingGuestSeat.classList.add('occupied');
+    let viewingGuestSeatId;
+    let viewingGuestSeat;
+    viewingGuestSeatId = document.querySelector('#viewing-guest-seat-id').value;
+    if(viewingGuestSeatId)
+        viewingGuestSeat = document.querySelectorAll(`.guest-placement__seat[data-seat-id="${viewingGuestSeatId}"]`)[0];
+    if(viewingGuestSeat)
+        viewingGuestSeat.classList.add('occupied');
 });
